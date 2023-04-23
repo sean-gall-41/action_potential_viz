2a01271daa4bbdcddc837d43c4dc8547054dcc3c
Sean Gallogly <galloglyst149@gmail.com>
2023-04-23 13:37:55 -0500

    scaffolding look and feel of the UI

--- src/main.js
+++ src/main.js
@@ -1,17 +1,37 @@
 const { invoke } = window.__TAURI__.tauri;
 
-let greetInputEl;
-let greetMsgEl;
+const plotArea = document.getElementById('plotting-area');
+const plotAreaWrapper = document.getElementById('canvas');
+const ctx = plotArea.getContext('2d');
 
-async function greet() {
-  // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-  greetMsgEl.textContent = await invoke("greet", { name: greetInputEl.value });
-}
+const N = 2000;
+const randElem = Array.from({length: N}, (_, id) => [id, Math.random()]);
+
+plotArea.width = plotAreaWrapper.getBoundingClientRect().width;
+ctx.strokeStyle = 'blue';
+ctx.lineWidth = 0.5;
+ctx.beginPath();
+
+randElem.forEach(([id, elem]) => {
+  let x = (id / N) * plotArea.width;
+  let y = elem * plotArea.height;
+  ctx.lineTo(x, y);
+});
+
+ctx.stroke();
+
+//let greetInputEl;
+//let greetMsgEl;
+
+//async function greet() {
+//  // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
+//  greetMsgEl.textContent = await invoke("greet", { name: greetInputEl.value });
+//}
 
 window.addEventListener("DOMContentLoaded", () => {
-  greetInputEl = document.querySelector("#greet-input");
-  greetMsgEl = document.querySelector("#greet-msg");
-  document
-    .querySelector("#greet-button")
-    .addEventListener("click", () => greet());
+  //greetInputEl = document.querySelector("#greet-input");
+  //greetMsgEl = document.querySelector("#greet-msg");
+  //document
+  //  .querySelector("#greet-button")
+  //  .addEventListener("click", () => greet());
 });
